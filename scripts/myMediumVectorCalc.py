caebbe711d58fdb89b4636fdd9f44f7e422abc24
AntonMIW <1065071@hr.nl>
2022-09-02 16:29:46 +0200

    einde dag 2 september weeeeekeeeend!!!

---
+++ scripts/myMediumVectorCalc.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# Dit programma rekent de som uit van twee vectoren (a en b) en slaat deze op in lijst (c)
+# de vectoren zijn lijsten
+# het programma neemt dus de corresponderende onderdelen van de lijsten (bv:1,1 en 2,1) 
+# en telt deze bij elkaar op en maakt hiervan de corresponderende cel in de nieuwe lijst (3,1)
+# dit programma controleert de vectoren op lengte, en past daarop de berekening aan, 
+# vectoren van verschillende lengte worden niet bij elkaar op geteld, want dat is nog te moeilijk en komt in myBigVectorCalc.py
+
+
+
+a = [0,3,1]
+b = [1,4,7]
+def sumVector (a,b):
+    for len (a)
+    if len(a)==len(b):
+        c = [a[0]+b[0],a[1]+b[1],a[2]+b[2]]
+        return c
+    else:
+        print("Rustaaaagh")
+
+
+
+print (sumVector(a,b))
+
+print (a)
+print (a,b)
+
+print (bin(231))
