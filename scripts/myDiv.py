a2fba6d1b4f16a92c22dc015a6e18456f9d95608
AntonMIW <1065071@hr.nl>
2022-09-02 11:31:34 +0200

    Script myDiv en beginnetje gemaakt aan functions

---
+++ scripts/myDiv.py
@@ -0,0 +1,15 @@
+
+
+def myDiv(a,b):
+	if (b==0):
+		return ("Da ken nie")
+	else:
+		return (a/b)
+    
+
+myDiv (1,2)
+print (myDiv (1,0))
+print (myDiv (0,1))
+print (myDiv ("kaas",0))
+print (myDiv (23,"Auto"))
+print (myDiv (kaas,0))
