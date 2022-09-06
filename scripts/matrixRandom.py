5a4e4fce32e77425cd4a5330721365c3a4b1bbfd
AntonMIW <1065071@hr.nl>
2022-09-06 12:02:25 +0200

    06 september tuples af

--- scripts/matrixRandom.py
+++ scripts/matrixRandom.py
@@ -4,36 +4,42 @@
 # En de waardes zijn gehele getallen (int)
 # Uiteindelijk krijgen we 1 lijst met daarin x maal y elementen verdeeld over x lijsten
 
+# Tussentijdse notitie: er wordt nu een lijst gemaakt van willekeurige lengte met willekeurige elementen, 
+# nu dus nog opdelen in lijstjes met de lengte van het aantal kolommen
+
+
 import random as randrange
 import random as ra
 import random
 
 
-low = -9
-high = 9
-in_r = random.randint(-9,9)
-col1 = random.randint(1,4)
-kol1 = random.randint(1,4)
-
-
-
-Rij1 = [random.choices(range(low,high), k=col1) for _ in range(kol1)]
-print(Rij1)
-
-
+matrixRows = ra.randint(1,4)
+matrixCollums = ra.randint(1,4)
 matrixRandomElements = ra.randint(-9,9)
+
 
 
 randomMatrix = []
 
 
-#def createTheRandomMatrix (matrixRows,matrixCollums):
-# for rows in range(matrixRows):
-#     for collums in range(matrixCollums):
-#         randomMatrix.append(matrixRandomElements)
+def createTheRandomMatrix (matrixRows,matrixCollums):
+    for rows in range(matrixRows):
+        for collums in range(matrixCollums):
+            randomMatrix.append(ra.randint(-9,9))
+    return randomMatrix
 
-# print(randomMatrix)
+print(createTheRandomMatrix (matrixRows,matrixCollums))
 
 
 
 
+# low = -9
+# high = 9
+# in_r = random.randint(-9,9)
+# col1 = random.randint(1,4)
+# kol1 = random.randint(1,4)
+
+
+
+# Rij1 = [random.choices(range(low,high), k=col1) for _ in range(kol1)]
+# print(Rij1)
