6b577bd86121e6425565b049fdef83702b252c16
AntonMIW <1065071@hr.nl>
2022-09-21 15:56:22 +0200

    21 september Lastig allemaal zeg...

---
+++ scripts/Opdrachten/examples/matrix/testMatrix.py
@@ -0,0 +1,23 @@
+import unittest
+from matrix import Matrix
+
+class TestMatrix(unittest.TestCase):
+
+
+
+
+    @classmethod
+    def setUpClass(cls):
+        print('setupClass')
+        
+        
+        
+        
+
+
+
+
+
+
+if __name__ == '__main__':
+    unittest.main()
