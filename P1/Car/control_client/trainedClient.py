a0c5bacc1a75ffdf2c0df336c05e3dc8920c785a
AntonMIW <1065071@hr.nl>
2022-10-07 13:00:45 +0200

    07 oktober P1 map

---
+++ P1/Car/control_client/trainedClient.py
@@ -0,0 +1,163 @@
+'''
+====== Legal notices
+
+Copyright (C) 2013 - 2021 GEATEC engineering
+
+This program is free software.
+You can use, redistribute and/or modify it, but only under the terms stated in the QQuickLicense.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY, without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+See the QQuickLicense for details.
+
+The QQuickLicense can be accessed at: http://www.qquick.org/license.html
+
+__________________________________________________________________________
+
+
+ THIS PROGRAM IS FUNDAMENTALLY UNSUITABLE FOR CONTROLLING REAL SYSTEMS !!
+
+__________________________________________________________________________
+
+It is meant for training purposes only.
+
+Removing this header ends your license.
+'''
+#pickle om het model in te kunnen voeren
+import pickle as pi
+#sklearn om het model te kunnen aanspreken
+import sklearn as sk
+from sklearn.neural_network import MLPClassifier
+#degene hieronder is denk ik overbodig?
+#from sklearn.preprocessing import StandardScaler
+#
+import time as tm
+import traceback as tb
+import math as mt
+import sys as ss
+import os
+import socket as sc
+import numpy as np
+ss.path +=  [os.path.abspath (relPath) for relPath in  ('..',)] 
+
+import socket_wrapper as sw
+import parameters as pm
+
+#getrainde model importeren met pickle
+sonarModel = pi.load(open('pickle_model.pkl', 'rb'))
+#lidarModel
+
+
+class TrainedClient:
+    def __init__ (self):
+        self.steeringAngle = 0
+        #model initialiseren
+        self.model = None
+        with open (pm.sampleFileName, 'w') as self.sampleFile:
+            with sc.socket (*sw.socketType) as self.clientSocket:
+                self.clientSocket.connect (sw.address)
+                self.socketWrapper = sw.SocketWrapper (self.clientSocket)
+                self.halfApertureAngle = False
+
+                while True:
+                    self.input ()
+                    self.sweep ()
+                    self.output ()
+                    self.logTraining ()
+                    tm.sleep (0.02)
+
+    def input (self):
+        sensors = self.socketWrapper.recv ()
+
+        if not self.halfApertureAngle:
+            self.halfApertureAngle = sensors ['halfApertureAngle']
+            self.sectorAngle = 2 * self.halfApertureAngle / pm.lidarInputDim
+            self.halfMiddleApertureAngle = sensors ['halfMiddleApertureAngle']
+            
+        if 'lidarDistances' in sensors:
+            self.lidarDistances = sensors ['lidarDistances']
+            # if self.model == None:
+            #     self.model = lidarModel
+        else:
+            self.sonarDistances = sensors ['sonarDistances']
+            if self.model == None:
+                self.model = sonarModel
+
+    def lidarSweep (self):
+        nearestObstacleDistance = pm.finity
+        nearestObstacleAngle = 0
+        
+        nextObstacleDistance = pm.finity
+        nextObstacleAngle = 0
+
+        for lidarAngle in range (-self.halfApertureAngle, self.halfApertureAngle):
+            lidarDistance = self.lidarDistances [lidarAngle]
+            
+            if lidarDistance < nearestObstacleDistance:
+                nextObstacleDistance =  nearestObstacleDistance
+                nextObstacleAngle = nearestObstacleAngle
+                
+                nearestObstacleDistance = lidarDistance 
+                nearestObstacleAngle = lidarAngle
+
+            elif lidarDistance < nextObstacleDistance:
+                nextObstacleDistance = lidarDistance
+                nextObstacleAngle = lidarAngle
+           
+        targetObstacleDistance = (nearestObstacleDistance + nextObstacleDistance) / 2
+
+        self.steeringAngle = (nearestObstacleAngle + nextObstacleAngle) / 2
+        self.targetVelocity = pm.getTargetVelocity (self.steeringAngle)
+#Hiero komt het model
+    def sonarSweep (self):
+        #Deze regel 
+        modelSteeringAngle = self.model.predict(np.array([self.sonarDistances]))
+
+        # deze regel iig nodig, bepaalt de snelheid adhv de stuurhoek        
+        self.targetVelocity = pm.getTargetVelocity(self.steeringAngle)        
+        # uitkomst van het model adhv input bepaalt de steering angle hieronder:        
+        self.steeringAngle = float(modelSteeringAngle[0])
+
+
+
+    def sweep (self):
+        if hasattr (self, 'lidarDistances'):
+            self.lidarSweep ()
+        else:
+            self.sonarSweep ()
+
+    def output (self):
+        actuators = {
+            'steeringAngle': self.steeringAngle,
+            'targetVelocity': self.targetVelocity
+        }
+
+        self.socketWrapper.send (actuators)
+
+    def logLidarTraining (self):
+        sample = [pm.finity for entryIndex in range (pm.lidarInputDim + 1)]
+
+        for lidarAngle in range (-self.halfApertureAngle, self.halfApertureAngle):
+            sectorIndex = round (lidarAngle / self.sectorAngle)
+            sample [sectorIndex] = min (sample [sectorIndex], self.lidarDistances [lidarAngle])
+
+        sample [-1] = self.steeringAngle
+        print (*sample, file = self.sampleFile)
+
+    def logSonarTraining (self):
+        sample = [pm.finity for entryIndex in range (pm.sonarInputDim + 1)]
+
+        for entryIndex, sectorIndex in ((2, -1), (0, 0), (1, 1)):
+            sample [entryIndex] = self.sonarDistances [sectorIndex]
+
+        sample [-1] = self.steeringAngle
+        print (*sample, file = self.sampleFile)
+
+    def logTraining (self):
+        if hasattr (self, 'lidarDistances'):
+            self.logLidarTraining ()
+        else:
+            self.logSonarTraining ()
+
+TrainedClient()
